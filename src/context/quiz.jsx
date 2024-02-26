affc936b85c6a7582bd072a9168bb1be2aa4b437
henriquexpaulo <phvieira3@gmail.com>
2024-02-25 22:57:51 -0300

    Quiz

---
+++ src/context/quiz.jsx
@@ -0,0 +1,58 @@
+import { createContext, useReducer} from "react";
+import questions from "../data/questions"
+
+const STAGES = ["Start", "Playing", "End"]
+
+const initialState = {
+    gameStage:STAGES[0],
+    questions,
+    currentQuestion: 0,
+    score: 0,
+}
+
+const quizReducer = (state, action) => {    
+    console.log(state, action)
+
+    switch(action.type){
+        case "CHANGE_STATE":
+        return {
+            ...state,
+            gameStage: STAGES[1],
+        };
+        
+        case "REORDER_QUESTIONS":
+        const reorderedQuestions = questions.sort(() => {
+            return Math.random() - 0.5;
+        });
+        return {
+            ...state,
+            questions: reorderedQuestions,
+        };
+
+        case "CHANGE_QUESTION":
+            const nextQuestion = state.currentQuestion + 1;
+            let endGame = false;
+
+            if(!questions[nextQuestion]){
+                endGame = true;
+            }
+
+        return{
+            ...state,
+            currentQuestion: nextQuestion,
+            gameStage: endGame ? STAGES[2] : state.gameStage,
+        }
+
+        default:
+            return state;
+    }
+    
+}
+
+export const QuizContext = createContext()
+
+export const QuizProvider = ({ children }) => {
+    const value = useReducer(quizReducer, initialState)
+
+    return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
+}
